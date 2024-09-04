0a31033f85d6385658749ecb469ab4cebdbef57c
jakub <jakub.janostik@digicomply.com>
2024-09-04 22:30:48 +0200

    Fixing issue with changes not propagating in the original array

--- core/pipeline/table.go
+++ core/pipeline/table.go
@@ -13,52 +13,53 @@ const maxOverlap = 0.8
 
 func (s *Segment) ParseTable() [][]Cell {
 
-	var cells []models.Prediction
+	var cells []*models.Prediction
 	for _, p := range s.Prediction.Table {
 		if p.Y1-p.Y0 < 16 {
 			// cleanup due to bad segmentation to handle small cells
 			// maybe we should add boundary to tables.
 			continue
 		}
-		cells = append(cells, p)
+		cells = append(cells, &p)
 	}
 
 	var overlappingCells []int
-	for index, _ := range cells {
-		var cell = cells[index]
-		for otherIndex, _ := range cells {
-			var otherCell = cells[otherIndex]
+	for index := range cells {
+		cell := cells[index]
+		for otherIndex := range cells {
+			otherCell := cells[otherIndex]
 			if otherIndex < index || contains(overlappingCells, otherIndex) {
 				continue
 			}
-			var overlap = Intersection(cell.Rect, otherCell.Rect)
+			overlap := Intersection(cell.Rect, otherCell.Rect)
 			if overlap > maxOverlap {
 				overlappingCells = append(overlappingCells, otherIndex)
 			} else if overlap > 0 {
-				var currentSmaller = Area(cell.Rect) <= Area(otherCell.Rect)
+				currentSmaller := Area(cell.Rect) <= Area(otherCell.Rect)
 				var cellToKeep *models.Prediction
 				var cellToAdjust *models.Prediction
 				if currentSmaller {
-					cellToKeep = &cell
-					cellToAdjust = &otherCell
+					cellToKeep = cell
+					cellToAdjust = otherCell
 				} else {
-					cellToKeep = &otherCell
-					cellToAdjust = &cell
+					cellToKeep = otherCell
+					cellToAdjust = cell
 				}
-				var diffs = [4]float32{
+				diffs := [4]float32{
 					absDiff(cellToKeep.X0, cellToAdjust.X1),
 					absDiff(cellToKeep.Y0, cellToAdjust.Y1),
 					absDiff(cellToKeep.X1, cellToAdjust.X0),
 					absDiff(cellToKeep.Y1, cellToAdjust.Y0),
 				}
-				var sideToAdjust = indexOfSmallest(diffs)
-				if sideToAdjust == 0 { // left side
+				sideToAdjust := indexOfSmallest(diffs)
+				switch sideToAdjust {
+				case 0: // left side
 					cellToAdjust.X1 = cellToKeep.X0
-				} else if sideToAdjust == 1 { // top side
+				case 1: // top side
 					cellToAdjust.Y1 = cellToKeep.Y0
-				} else if sideToAdjust == 2 { // right side
+				case 2: // right side
 					cellToAdjust.X0 = cellToKeep.X1
-				} else if sideToAdjust == 3 { // bottom side
+				case 3: // bottom side
 					cellToAdjust.Y0 = cellToKeep.Y1
 				}
 			}
@@ -67,7 +68,7 @@ func (s *Segment) ParseTable() [][]Cell {
 
 	s.Prediction.Table = make([]models.Prediction, len(cells))
 	for i, cell := range cells {
-		s.Prediction.Table[i] = cell
+		s.Prediction.Table[i] = *cell
 	}
 	//s.Prediction.Table = cells
 
