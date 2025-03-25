3383f0d585e39db057cd0b25913efdbe7822bb9c
jakub <jakub.janostik@digicomply.com>
2025-03-25 09:32:53 +0100

    Ensure data files are created

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"smart-docs/core/server"
 
 	"github.com/joho/godotenv"
@@ -11,6 +12,17 @@ import (
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Printf("Warning: Error loading .env file: %v", err)
+	}
+
+	dirs := []string{
+		"./data/files",
+		"./data/images",
+	}
+
+	for _, dir := range dirs {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			panic(fmt.Sprintf("Cannot create directory %s: %v", dir, err))
+		}
 	}
 
 	newServer := server.NewServer()
